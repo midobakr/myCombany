1d5e263830ce3eef1a9fe245816eda77c8760514
bakr <you@example.com>
2021-01-29 21:13:11 +0200

    finshed registeredUsers component"

--- my-app/src/Dashboard/components/registeredUsers/registeredUsers.js
+++ my-app/src/Dashboard/components/registeredUsers/registeredUsers.js
@@ -1,16 +1,38 @@
-import {useEffect} from 'react'
+import {useEffect , useState} from 'react'
 import {connect} from "react-redux";
 import classes from './registeredUsers.module.css'
 import getAsync from "../../../store/actions/getAsync";
 import {SET_REGISTEREDUSERS} from "../../../store/actions/actions";
 
+let days = ['Sunday' , 'Monday' , 'Tuesday' , 'Wendsday','Thrusday','Friday' , 'Saturday']
+
 function RegisteredUsers({registeredUsers , getRegisteredUsers}) {
-    
+    let todayDate = new Date().toJSON().split('T')[0]
+    let [selectedDate , setSelectedDate] = useState(todayDate)
     useEffect(()=>{
-        getRegisteredUsers()
-    },[getRegisteredUsers])
+        getRegisteredUsers(selectedDate)
+    },[getRegisteredUsers , selectedDate])
+    
+    const getPreviousDayDate =(num)=>{
+        const today = new Date(selectedDate)
+        const yestarday  =new Date(today.setDate(today.getDate()-num))
+        return yestarday.toJSON().split('T')[0]
+    }
     return(
         <div className={classes.container}>
+            <div style={{display:'inline-block' , position:'relative'}}>
+            <input max={todayDate} 
+                   onChange={(e)=>{setSelectedDate(e.target.value)}} 
+                   type='Date' 
+                   Value={selectedDate} 
+                   className={classes.dateInput}/>
+            <div style={{position:'absolute' ,display:'flex',alignItems:'center', right:'40px', bottom:'0' , height:'30px'}}>{days[new Date(selectedDate).getDay()]}</div>
+            </div>
+           <div style={{display:'flex' ,justifyContent:'space-between' , width:'50%' , margin:'0 auto'}}>
+            <h3 style={{cursor:'pointer',visibility:todayDate ===selectedDate ? 'hidden' : 'visible'}} onClick={()=>{setSelectedDate(getPreviousDayDate(-1))}} >next day</h3>
+            <h3 style={{cursor:'pointer'}} onClick={()=>{setSelectedDate(getPreviousDayDate(1))}} >previous day</h3>
+
+            </div>
         {registeredUsers[0] ?
                     registeredUsers.map((record)=>{
                     let today_record = new Date(record.AttendAt)
@@ -18,8 +40,8 @@ function RegisteredUsers({registeredUsers , getRegisteredUsers}) {
                         return(
                             
                  <div className={classes.box}>
-                <img style={{borderRadius:'50%'}} src={`https://www.gravatar.com/avatar/${record.avatar}`} alt='profile ' />
-                <h2 style={{textAlign:'center'}}>{record.name}</h2>
+                <img style={{borderRadius:'50%' , width:'80px' , height:'80px'}} src={`https://www.gravatar.com/avatar/${record.avatar}`} alt='profile ' />
+                <h2 style={{textAlign:'center' , width:'5em'}}>{record.name}</h2>
                 <h2>
                     <span>{today_record.getHours()>12 ?
                             today_record.getHours()-12 +':'+
@@ -42,7 +64,7 @@ function RegisteredUsers({registeredUsers , getRegisteredUsers}) {
                     })
                 
             :
-            <h1 style={{textAlign:'center'}}>there is no Users registered yet !!</h1>
+            <h3 style={{textAlign:'center ' , margin:'auto'}}>there is no Users registered on that day</h3>
                 
             
                  
@@ -55,7 +77,7 @@ const mapStateToProps = (state)=>({
     registeredUsers : state.registeredUsers
 })
 const mapDispatchToProps = (dispatch)=>({
-getRegisteredUsers : (id)=>dispatch(getAsync('/dashboard/registeredUsers/' , SET_REGISTEREDUSERS))
+getRegisteredUsers : (todayDate)=>dispatch(getAsync('/dashboard/registeredUsers/'+todayDate , SET_REGISTEREDUSERS))
 
 })
 
